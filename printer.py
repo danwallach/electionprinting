5ca91664f2f932f5e7f4a7592ad8618d81805b3c
Lauren Wood <woodl@Laurens-MacBook-Pro-2.local>
2016-10-17 16:30:34 -0500

    initial commit

---
+++ printer.py
@@ -0,0 +1,20 @@
+import sys
+from sys import argv
+
+def main():
+	usage = 'Command Syntax: \npython printer.py input_filename \nArguments:\n\tinput_filename\tfile of voters to format to PDF\n'
+	if (len(argv) >= 2 and argv[1] == '-h') or len(argv) <= 1 or len(argv) > 2:
+	    print usage
+	elif len(argv) == 2:
+		# print PDFs
+		print_pdfs(argv[1])
+
+
+def print_pdfs(filename):
+
+	print(filename)
+
+
+
+
+main()
