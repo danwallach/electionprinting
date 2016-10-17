d0571a577e8f9ad47bf75fdf27617f23ecc5a690
Lauren Wood <woodl@Laurens-MacBook-Pro-2.local>
2016-10-17 16:49:27 -0500

    fixed for real

--- printer.py
+++ printer.py
@@ -2,7 +2,7 @@ import sys
 from sys import argv
 
 def main():
-	usage = 'Command Syntax: ./printer input_filename \nArguments:\n\tinput_filename\tfile of voters to format to PDF\n'
+	usage = 'Command Syntax: \n\t./printer input_filename \nArguments:\n\tinput_filename\tfile of voters to format to PDF\n'
 	if (len(argv) >= 2 and argv[1] == '-h') or len(argv) <= 1 or len(argv) > 2:
 	    print usage
 	elif len(argv) == 2:
