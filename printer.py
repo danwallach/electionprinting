ac39b2303804b28c843faaec2381d0e0daa33ab1
Ashley Gentles <arg7@rice.edu>
2016-11-21 15:26:57 -0600

    reads in file of race results

--- printer.py
+++ printer.py
@@ -20,7 +20,9 @@ from functools import partial
 
 """
 1) create a way to read race info from a file
+
 	what type of file? STAR link in here?
+
 2) anchor barcode in bottom left
 3) make num_rows depend on amount of text 
 	can we check the height of paragraph to determine when a col gets too long
@@ -73,15 +75,15 @@ class SelectionInfo:
 
 
 def main():
-	usage = 'Command Syntax: \n\t./printer input_filename num_columns\nArguments:\n\tinput_filename\tfile to save results to\n\tnum_columns\tnumber of columns for PDF\n'
-	if argv[1] == '-h' or len(argv) <= 1 or len(argv) > 3:
+	usage = 'Command Syntax: \n\t./printer input_filename num_columns\nArguments:\n\tinput_filename\tfile to save results to\n\tnum_columns\tnumber of columns for PDF\n\traces_filename\tsemi-colon delimited list of race results\n'
+	if argv[1] == '-h' or len(argv) <= 1 or len(argv) > 4:
 	    print(usage)
-	elif len(argv) == 3:
+	elif len(argv) == 4:
 		# print PDFs
-		print_pdfs(argv[1], argv[2])
+		print_pdfs(argv[1], argv[2], argv[3])
 
 
-def print_pdfs(filename, num_columns):
+def print_pdfs(filename, num_columns, races):
 
 	config = ConfigParser.ConfigParser()
 	config.read('config.cfg')
@@ -103,61 +105,12 @@ def print_pdfs(filename, num_columns):
 	style_right = ParagraphStyle(name='right', parent=styles['Normal'], alignment=TA_RIGHT)
 	style_right.fontSize = font_size
 
-	
-	results = []
-	races = [("President and Vice President", "Hillary Clinton / Tim Kaine", "DEM"),
-		("Representative, District 2", "James B. Veasaw", "LIB"),
-		("Railroad Commissioner", "Grady Yarbrough", "DEM"),
-		("Justice, Supreme Court, Place 3", "Rodolfo Rivera Munoz", "GP"),
-		("Justice, Supreme Court, Place 5", "Dori Contreras Garza", "GP"),
-		("Justice, Supreme Court, Place 9", "Savannah Robinson", "DEM"),
-		("Judge, Court of Criminal Appeals, Place 2", "Lawrence Larry Meyers", "DEM"),
-		("Judge, Court of Criminal Appeals, Place 5", "William Bryan Strange, III", "LIB"),
-		("Judge, Court of Criminal Appeals, Place 6", "Michael E. Keasler", "DEM"),
-		("Member, State Board of Education, District 6" , "Donna Bahorich", "REP"),
-		("State Senator, District 13" , "Borris L. Miles", "DEM"),
-		("State Representative, District 134" , "Gilberto \"Gil\" Velasquez Jr.", "LIB"),
-		("Chief Justice, 1st Court of Appeals" , "Sherry Radack", "REP"),
-		("Justice, 14th Court of Appeals District, Place 2" , "Candace White", "DEM"),
-		("Justice, 14th Court of Appeals District, Place 9" , "Tracy Elizabeth Christopher", "REP"),
-		("District Judge, 11th Judicial District", "Kristen Hawkins", "DEM"),
-		("District Judge, 61st Judicial District", "Erin Elizabeth Lunceford", "REP"),
-		("District Judge, 80th Judicial District", "Larry Weiman", "DEM"),
-		("District Judge, 125th Judicial District", "Sharon Hemphill", "REP"),
-		("District Judge, 127th Judicial District", "Sarahjane Swanson", "REP"),
-		("District Judge, 129th Judicial District", "Michael Gomez", "DEM"),
-		("District Judge, 133rd Judicial District", "Cindy Bennett Smith", "REP"),
-		("District Judge, 151st Judicial District", "Mike Englehart", "DEM"),
-		("District Judge, 152nd Judicial District", "Robert K. Schaffer", "DEM"),
-		("District Judge, 164th Judicial District", "Alexandra Smoots-Hogan", "DEM"),
-		("District Judge, 165th Judicial District", "Debra Ibarra Mayfield", "REP"),
-		("District Judge, 174th Judicial District", "Katherine McDaniel", "REP"),
-		("District Judge, 176th Judicial District", "Nikita \"Niki\" Harmon", "DEM"),
-		("District Judge, 177th Judicial District", "Robert	Johnson", "DEM"),
-		("District Judge, 178th Judicial District", "Phil Gommels", "REP"),
-		("District Judge, 179th Judicial District", "Kristin M. Guiney", "REP"),
-		("District Judge, 215th Judicial District", "Fred Schuchart", "REP"),
-		("District Judge, 333rd Judicial District", "Joseph \"Tad\"	Halbach", "REP"),
-		("District Judge, 334th Judicial District", "Steven Kirkland", "DEM"),
-		("District Judge, 337th Judicial District", "Renee Magee", "REP"),
-		("District Judge, 338th Judicial District", "Ramona Franklin", "DEM"),
-		("District Judge, 339th Judicial District", "Maria T. (Terri) Jackson", "DEM"),
-		("District Judge, 351st Judicial District", "Mark Kent Ellis", "REP"),
-		("District Judge, 507th Judicial District", "Julia Maldonado", "DEM"),
-		("District Attorney", "Devon Anderson", "REP"),
-		("Judge, County Civil Court at Law No. 1 (Unexpired Term)", "Clyde Raymond Leuchtag", "REP"),
-		("Judge, County Criminal Court No. 16", "Darrell William Jordan", "DEM"),
-		("County Attorney", "Jim Leitner", "REP"),
-		("Sheriff", "Ed Gonzalez", "DEM"),
-		("County Tax Assessor-Collector", "Ann Harris Bennett", "DEM"),
-		("County Commissioner, Precinct 1", "Rodney Ellis", "DEM"),
-		("Justice of the Peace, Precinct 1, Place 1", "Eric William Carter", "DEM"),
-		("Constable, Precint 1", "Joe Danna", "REP"),
-		("Houston I.S.D., Proposition 1", "AGAINST", "")]
 
 	results = []
-	for item in races:
-		results.append(SelectionInfo(item[0], item[1], item[2]))
+	f = open(races, "r")
+	for item in f.readlines():
+		item = item.split(";")
+		results.append(SelectionInfo(item[0], item[1], item[2].strip("\n")))
 
 	#num_rows = math.ceil(len(results)/int(num_columns))
 	num_rows = 12
@@ -167,7 +120,7 @@ def print_pdfs(filename, num_columns):
 	candidate_index = 0
 	column_index = 0
 
-	num_colums_total = math.ceil(len(races)/num_rows)
+	num_colums_total = math.ceil(len(results)/num_rows + 1)
 	num_pages_total = math.ceil(num_colums_total/ncols)
 
 	print("num cols total %i, num pages total %i" %(num_colums_total, num_pages_total))
@@ -201,11 +154,12 @@ def print_pdfs(filename, num_columns):
 				if candidate_index >= len(results):
 					break
 
+
 			col_table = Table(new_col)
 			data[0].append(col_table)
 			column_index += 1
 			if column_index >= num_colums_total:
-					break
+				break
 
 		column_widths = [8*inch/ncols] * ncols
 
