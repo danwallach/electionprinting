a8bc853f4746adf0720d66f1af54d1b551f8a88e
Max Hasbrouck <maxhasbro@gmail.com>
2016-11-29 20:50:23 -0600

    removed print statements, add legal paper size support

--- printer.py
+++ printer.py
@@ -22,20 +22,6 @@ from reportlab.lib.units import mm
 from functools import partial
 
 
-
-"""
-
-1) make num_rows depend on amount of text 
-	can we check the height of paragraph to determine when a col gets too long
-2) decide what is reasonable in config file
-3) look into human testing readability limits and consider making chart
-4) create documentation
-
-THOUGHTS: can we be more descriptive with the invalid input statement
-	 i.e decrease num_columns, adjust font_size?
-
-"""
-
 styles = getSampleStyleSheet()
 styleN = styles["BodyText"]
 styleN.alignment = TA_LEFT
@@ -45,31 +31,31 @@ font_size = 7
 font_type = 'Times-Roman'
  
 def header_footer(canvas, doc):
-    canvas.saveState()
-    # Footer
-    barcode = Image("barcode1.jpg")
-    barcode.drawHeight = 2.25*inch*barcode.drawHeight / barcode.drawWidth
-    barcode.drawWidth = 2.25*inch
-    barcode.hAlign = 'LEFT'
-    barcode.vAlign = 'BOTTOM'
+	canvas.saveState()
+	# Footer
+	barcode = Image("barcode1.jpg")
+	barcode.drawHeight = 2.25*inch*barcode.drawHeight / barcode.drawWidth
+	barcode.drawWidth = 2.25*inch
+	barcode.hAlign = 'LEFT'
+	barcode.vAlign = 'BOTTOM'
 
-    w, h = barcode.wrap(doc.width, doc.bottomMargin)
-    barcode.drawOn(canvas, doc.leftMargin + 15, h)
+	w, h = barcode.wrap(doc.width, doc.bottomMargin)
+	barcode.drawOn(canvas, doc.leftMargin + 15, h)
 
-    # Header
-    header = []
-    header_style_right = ParagraphStyle(name='right', parent=styles['Normal'], alignment=TA_RIGHT)
-    try:
-    	header.append([Paragraph("<font size=12><b>Official Ballot</b></font><font size=8><br/>November 8, 2016 General Election<br/>Harris County, Texas Precinct 101A </font>", styleN), [barcode, Paragraph('<b><font size=15 name="' + font_type + '">PLACE THIS IN BALLOT BOX</font></b>', header_style_right)]])
-    except:
-		print 'Error creating header. This font, ' + font_type + ', may not be allowed.'
+	# Header
+	header = []
+	header_style_right = ParagraphStyle(name='right', parent=styles['Normal'], alignment=TA_RIGHT)
+	try:
+		header.append([Paragraph("<font size=12><b>Official Ballot</b></font><font size=8><br/>November 8, 2016 General Election<br/>Harris County, Texas Precinct 101A </font>", styleN), [barcode, Paragraph('<b><font size=15 name="' + font_type + '">PLACE THIS IN BALLOT BOX</font></b>', header_style_right)]])
+	except:
+		print('Error creating header. This font, ' + font_type + ', may not be allowed.')
 		return
-    header = Table(header, colWidths=[inch*3, inch*5], style=[('FONTSIZE', (0, 0), (-1, -1), 50), ('TEXTFONT', (0, 0), (1, 0), font_type), ('ALIGN',(1,0),(1,0),'RIGHT')])
+	header = Table(header, colWidths=[inch*3, inch*5], style=[('FONTSIZE', (0, 0), (-1, -1), 50), ('TEXTFONT', (0, 0), (1, 0), font_type), ('ALIGN',(1,0),(1,0),'RIGHT')])
 
-    w, h = header.wrap(doc.width, doc.topMargin)
-    header.drawOn(canvas, doc.leftMargin + 15, doc.height + doc.topMargin - h)
+	w, h = header.wrap(doc.width, doc.topMargin)
+	header.drawOn(canvas, doc.leftMargin + 15, doc.height + doc.topMargin - h)
 
-    canvas.restoreState()
+	canvas.restoreState()
 
 
 FONTSIZE = 0
@@ -85,7 +71,7 @@ class SelectionInfo:
 def main():
 	usage = 'Command Syntax: \n\t./printer input_filename\nArguments:\n\tinput_filename\tfile to save results to\n'
 	if argv[1] == '-h' or len(argv) <= 1 or len(argv) > 2:
-	    print(usage)
+		print(usage)
 	else:
 		# print PDFs
 		print_pdfs(argv[1])
@@ -108,6 +94,9 @@ def print_pdfs(filename):
 	page_type = letter
 	if page_size == 'Legal':
 		page_type = legal
+		page_height = 700
+	else:
+		page_height = 500
 
 
 	ncols = int(num_columns)
@@ -132,14 +121,7 @@ def print_pdfs(filename):
 		return
 
 
-	#num_rows = math.ceil(len(results)/int(num_columns))
-	#num_rows = 12
-
-	print(len(results))
-	
 	candidate_index = 0
-
-	#num_colums_total = math.ceil(len(results)/num_rows + 1)
 
 
 	frame = Frame(doc.leftMargin, doc.bottomMargin, doc.width, doc.height-20*mm, id='normal2')
@@ -148,59 +130,51 @@ def print_pdfs(filename):
 
 	elements = []
 	entries = 0
-	print(len(results))
+	bigH = 0
+	#print("Adding {} entries".format(len(results)))
 	while entries < len(results):
 
 		data = [[]]
 
-		print('entries: ' + str(entries))
 
 		for i in range(ncols):
 			new_col = []
 			tot_h = 0
-			while tot_h < 500:
+			while tot_h < page_height:
 				
 				try:
 					race_name = Paragraph("<b>"+results[candidate_index].race_name+"</b>", styleN)
 					selection_name = Paragraph(results[candidate_index].selection, styleN)
 					party = Paragraph("<b>"+results[candidate_index].party+"</b>", style_right)
 				except:
-					print 'Error bolding text. This font may not be allowed, or it may not allow bolded text.'
+					print('Error bolding text. This font may not be allowed, or it may not allow bolded text.')
 					return
 
 				race_data = [[race_name], [selection_name, party]]
 
-				race_table = Table(race_data, colWidths=[inch*7.5/ncols*24/32, inch*7.5/ncols*9/32], \
+				race_table = Table(race_data, colWidths=[inch*7.5/ncols*23/32, inch*7.5/ncols*10/32], \
 					style=[('SPAN',(0,0),(1,0)), ('LINEBELOW', (0,1), (1,1), 1, colors.black), ('FONTSIZE', (0, 0), (-1, -1), 3), ('FONTNAME', (0, 0), (-1, -1), font_type)])
 				w,h = race_table.wrap(0,0)
 				tot_h += h
 				new_col.append([race_table])
 				candidate_index += 1
 				entries += 1
-				print("len: {}, candidate_index: {}".format(len(results), candidate_index))
 				if candidate_index > (len(results) - 1):
-					print("broken")
 					break
 			col_table = Table(new_col)
 			data[0].append(col_table)
 			if candidate_index > (len(results) - 1):
-				print('candidate_index >= len results: ' + str(candidate_index))
+				#print('candidate_index >= len results: ' + str(candidate_index))
 				break
 			
 		column_widths = [8*inch/ncols] * ncols
 
 		t=Table(data,colWidths=inch*8/ncols, style=[('VALIGN',(0,0), (-1, -1), 'TOP')],hAlign='LEFT')
 		
-		#elements.append(header)
 		elements.append(t)
-		#TODO: ensure this barcode is aligned to bottom of page, for example see 3 col with 59 races
-		#elements.append(barcode)
 		elements.append(NextPageTemplate('header_footer'))
 		elements.append(PageBreak())
 		
-	#doc.addPageTemplates([template])
-
-	#doc.build(elements, onFirstPage=self._header_footer, onLaterPages=self._header_footer, canvasmaker=NumberedCanvas)
 	try:
 		doc.build(elements)
 	except:
