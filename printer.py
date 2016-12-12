9ffd694cf9fbadf9ce496ce521f90b59b83607cb
Lauren Wood <woodl@Laurens-MacBook-Pro-3.local>
2016-12-11 19:00:36 -0600

    added more commenting, and checks for the barcode input number

--- printer.py
+++ printer.py
@@ -30,6 +30,7 @@ styles = getSampleStyleSheet()
 styleN = styles["BodyText"]
 styleN.alignment = TA_LEFT
 
+# default values
 page_size = letter
 font_size = 7
 font_type = 'Times-Roman'
@@ -40,39 +41,20 @@ def header_footer(canvas, doc):
 	global page_num
 
 	canvas.saveState()
+
 	# Footer
-	# THIS IS WHERE WE CONVERT SVG TO A "ReportLab Graphics Drawing"
 
+	# Create Barcode and increment page num
 	bar_num_page = barcode_num_saved + str(page_num)
-	print bar_num_page
 	page_num += 1
-
 	ean = barcode.get('upc', bar_num_page)
-
-
-	
 	barcode_file_name = ean.save('upc' + bar_num_page)
-	print barcode_file_name
-
-
+	# converted to "ReportLab Graphics Drawing"
 	barcode_drawing = svg2rlg(barcode_file_name)
-
-
-
-	# WHAT WE USED TO DO
-	#barcode = Image("barcode1.jpg")
-
-	# NOT SURE IF WE CAN DO THIS WITH A DRAWING
-	# barcode.drawHeight = 2.25*inch*barcode.drawHeight / barcode.drawWidth
-	# barcode.drawWidth = 2.25*inch
-	# barcode.hAlign = 'LEFT'
-	# barcode.vAlign = 'BOTTOM'
 
 	w, h = barcode_drawing.wrap(doc.width, doc.bottomMargin)
 
-	# WHAT WE USED TO DO "drawImage" WAS SOMETHING ELSE I TRIED
 	barcode_drawing.drawOn(canvas, doc.leftMargin + 15, 15)
-	#canvas.drawImage(barcode, doc.leftMargin + 15, h)
 
 	# Header
 	header = []
@@ -109,12 +91,16 @@ def main():
 	else:
 		# print PDFs
 		barcode_num_saved = argv[2]
+		if len(barcode_num_saved) >= 11:
+			print('\nERROR: Number for barcode is too long. Automatically truncating to 10 digitss. \nIf this is not inteded behavior please change barcode number.\n')
+			barcode_num_saved = barcode_num_saved[0:10]
 		print_pdfs(argv[1])
 
 
 def print_pdfs(filename):
 	global font_size, font_type, barcode_file_name
 
+	# read in configurations
 	config = ConfigParser()
 	config.read('config.cfg')
 	page_size = config.get('Paper', 'size')
@@ -126,6 +112,7 @@ def print_pdfs(filename):
 	styleN.fontSize = font_size
 	styleN.fontName = font_type
 
+	# determine page size
 	page_type = letter
 	if page_size == 'Legal':
 		page_type = legal
@@ -138,11 +125,12 @@ def print_pdfs(filename):
 
 	doc = SimpleDocTemplate(filename, pagesize=page_type, topMargin=15, bottomMargin=15, leftMargin=0, rightMargin=15)
 
+	# create styles
 	style_right = ParagraphStyle(name='right', parent=styles['Normal'], alignment=TA_RIGHT)
 	style_right.fontSize = font_size
 	style_right.fontName = font_type
 
-
+	# read in races
 	results = []
 	try:
 		f = open(races, "r")
@@ -158,7 +146,7 @@ def print_pdfs(filename):
 
 	candidate_index = 0
 
-
+	# Set up page
 	frame = Frame(doc.leftMargin, doc.bottomMargin, doc.width, doc.height-30*mm, id='normal2')
 	template = PageTemplate(id='header_footer', frames=frame, onPage=header_footer)
 	doc.addPageTemplates([template])
@@ -166,21 +154,18 @@ def print_pdfs(filename):
 	elements = []
 	entries = 0
 	bigH = 0
-	#print("Adding {} entries".format(len(results)))
 
+	# add entries to the page
 	while entries < len(results):
-
 		data = [[]]
-
-		# generate barcode for this page
-		# THIS IS BARCODE STUFF I ADDED, SHOULD MAKE NEW BARCODE EACH TIME
-		
-
 		for i in range(ncols):
 			new_col = []
 			tot_h = 0
+
+			# while there is still room in this column 
 			while tot_h < page_height:
 				
+				# Format paragraphs for this race
 				try:
 					race_name = Paragraph("<b>"+results[candidate_index].race_name+"</b>", styleN)
 					selection_name = Paragraph(results[candidate_index].selection, styleN)
@@ -189,8 +174,10 @@ def print_pdfs(filename):
 					print('Error bolding text. This font may not be allowed, or it may not allow bolded text.')
 					return
 
+				# Create race data list from those paragraph
 				race_data = [[race_name], [selection_name, party]]
 
+				# form table from the race data information: 1 table per race that goes inside larger table
 				race_table = Table(race_data, colWidths=[inch*7.5/ncols*23/32, inch*7.5/ncols*10/32], \
 					style=[('SPAN',(0,0),(1,0)), ('LINEBELOW', (0,1), (1,1), 1, colors.black), ('FONTSIZE', (0, 0), (-1, -1), 3), ('FONTNAME', (0, 0), (-1, -1), font_type)])
 				w,h = race_table.wrap(0,0)
@@ -198,18 +185,20 @@ def print_pdfs(filename):
 				new_col.append([race_table])
 				candidate_index += 1
 				entries += 1
+
+				# we've added them all
 				if candidate_index > (len(results) - 1):
 					break
+
 			col_table = Table(new_col)
 			data[0].append(col_table)
 			if candidate_index > (len(results) - 1):
-				#print('candidate_index >= len results: ' + str(candidate_index))
 				break
 			
 		column_widths = [8*inch/ncols] * ncols
 
+		# Create table with all this information and add to this page, adding header and footer as well
 		t=Table(data,colWidths=inch*8/ncols, style=[('VALIGN',(0,0), (-1, -1), 'TOP')],hAlign='LEFT')
-		
 		elements.append(t)
 		elements.append(NextPageTemplate('header_footer'))
 		elements.append(PageBreak())
@@ -217,7 +206,7 @@ def print_pdfs(filename):
 	try:
 		doc.build(elements)
 	except:
-		print("invalid inputs")
+		print("Error building the document, unable to determine cause.")
 
 
 main()
